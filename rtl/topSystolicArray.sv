1e9cc39aae5ebc0f38c2b3c92556b627bc88f06c
hieuledinh <work.hieuld@gmail.com>
2025-03-13 19:36:55 +0700

    rename gen-block and localparam

--- rtl/topSystolicArray.sv
+++ rtl/topSystolicArray.sv
@@ -1,6 +1,6 @@
 //`default_nettype none
 module topSystolicArray #(
-    parameter int unsigned N = 8
+    parameter int unsigned N = 16
 ) (
     input  logic                      i_clk,
     input  logic                      i_arst,
@@ -15,11 +15,12 @@ module topSystolicArray #(
 
   logic doProcess_d, doProcess_q;
 
-  localparam bit N_VALID = &{N > 2, N < 257};
+  // localparam bit Nvalid = &{N > 2, N < 257};
+  localparam bit Nvalid = (N > 2) && (N < 257);
 
-  if (!N_VALID) begin : ParamCheck
+  if (!Nvalid) begin : gen_ParaCheck
     $error("Matrix dimension size 'N' is invalid.");
-  end : ParamCheck
+  end : gen_ParaCheck
 
   // }}} Check matrix dimension size is valid
 
@@ -28,11 +29,11 @@ module topSystolicArray #(
   // the necessary control signals.
 
   // Number of clock cycles required to complete matrix multiplication.
-  localparam int unsigned MULT_CYCLES = 3 * N - 2;
+  localparam int unsigned MultCycles = 3 * N - 2;
   // `+1` to support counter_q + 1;
-  localparam int unsigned MULT_CYCLES_W = $clog2(MULT_CYCLES + 1);
+  localparam int unsigned MultCyclesWidth = $clog2(MultCycles + 1);
 
-  logic [MULT_CYCLES_W-1:0] counter_d, counter_q;
+  logic [MultCyclesWidth-1:0] counter_d, counter_q;
 
   always_ff @(posedge i_clk, posedge i_arst) begin
     if (i_arst) begin
@@ -41,7 +42,6 @@ module topSystolicArray #(
       counter_q <= counter_d;
     end
   end
-
   always_comb begin
     if (doProcess_d == '1) begin
       counter_d = counter_q + 1'b1;
@@ -57,7 +57,7 @@ module topSystolicArray #(
   always_ff @(posedge i_clk, posedge i_arst) begin
     if (i_arst) begin
       validResult_q <= '0;
-    end else if (counter_q == MULT_CYCLES_W'(MULT_CYCLES)) begin
+    end else if (counter_q == MultCyclesWidth'(MultCycles)) begin
       validResult_q <= '1;
     end else begin
       validResult_q <= '0;
@@ -79,24 +79,21 @@ module topSystolicArray #(
   always_comb begin
     if (i_validInput) begin
       doProcess_d = '1;
-    end else if (counter_q == MULT_CYCLES_W'(MULT_CYCLES + 1)) begin
+    end else if (counter_q == MultCyclesWidth'(MultCycles + 1)) begin
       doProcess_d = '0;
     end else doProcess_d = doProcess_q;
-  end
-  // }}} Systolic array clock gate
-
-  // {{{ Set-up row and column matrices
-
+  end  // }}} Systolic array clock gate {{{ Set-up row and column matrices
   localparam int unsigned PAD = 8 * (N - 1);  //N = 4 -> 24 bits padding
-  localparam bit [PAD-1:0] APPEND_ZERO = PAD'(0);
+  localparam bit [PAD-1:0] AppendZero = PAD'(0);
 
   logic [N-1:0][(2*N)-2:0][7:0] row_d, row_q;
+
   logic [N-1:0][(2*N)-2:0][7:0] col_d, col_q;
 
   logic [N-1:0][N-1:0][7:0] invertedRowElements;
   logic [N-1:0][N-1:0][7:0] invertedColElements;
 
-  for (i = 0; i < N; i++) begin : perRowCol
+  for (i = 0; i < N; i++) begin : gen_perRowCol
 
     always_ff @(posedge i_clk, posedge i_arst) begin
       if (i_arst) begin
@@ -107,7 +104,7 @@ module topSystolicArray #(
     end
     always_comb begin
       if (i_validInput) begin
-        row_d[i] = {APPEND_ZERO, invertedRowElements[i]} << i * 8;
+        row_d[i] = {AppendZero, invertedRowElements[i]} << i * 8;
       end else if (counter_q != '0) begin
         row_d[i] = row_q[i] >> 8;
       end else begin
@@ -115,11 +112,11 @@ module topSystolicArray #(
       end
     end
     // Invert the positions of the elements in each row to form the row matrix.
-    for (j = 0; j < N; j++) begin : perRowElement
+    for (j = 0; j < N; j++) begin : gen_perRowElement
 
       assign invertedRowElements[i][j] = i_a[i][N-j-1];
 
-    end : perRowElement
+    end : gen_perRowElement
 
     always_ff @(posedge i_clk, posedge i_arst) begin
       if (i_arst) begin
@@ -130,7 +127,7 @@ module topSystolicArray #(
     end
     always_comb begin
       if (i_validInput) begin
-        col_d[i] = {APPEND_ZERO, invertedColElements[i]} << i * 8;
+        col_d[i] = {AppendZero, invertedColElements[i]} << i * 8;
       end else if (counter_q != '0) begin
         col_d[i] = col_q[i] >> 8;
       end else begin
@@ -138,13 +135,13 @@ module topSystolicArray #(
       end
     end
     // Invert the positions of the elements in each col to form the col matrix.
-    for (j = 0; j < N; j++) begin : perColElement
+    for (j = 0; j < N; j++) begin : gen_perColElement
 
       assign invertedColElements[i][j] = i_b[N-j-1][i];
 
-    end : perColElement
+    end : gen_perColElement
 
-  end : perRowCol
+  end : gen_perRowCol
 
   // }}} Set-up rows and columns matrices
 
